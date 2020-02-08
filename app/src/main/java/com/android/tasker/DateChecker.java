11431671f0b03fa36dfb41ddfebc34e967ef5bb1
bumis95 <bumis95@gmail.com>
2020-02-08 12:55:16 +0300

    переработка классов
    правильное отображение текущего дня

--- app/src/main/java/com/android/tasker/DateChecker.java
+++ app/src/main/java/com/android/tasker/DateChecker.java
@@ -9,13 +9,37 @@ import java.util.Locale;
 public class DateChecker {
 
     static Calendar calendarNow = Calendar.getInstance();
-    static Calendar calendarStart = Calendar.getInstance();
+    static Calendar calendarLastComplete = Calendar.getInstance();
 
-    public static int check(int currentDay, String dateStart) {
+    public static int check(String dateLastComplete) {
 
         String dateNow = //"02.01.20";
                 new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(calendarNow.getTime());
 
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+
+        Date dateOne = null;
+        Date dateTwo = null;
+        try {
+            dateOne = format.parse(dateNow);
+            dateTwo = format.parse(dateLastComplete);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        calendarNow.setTime(dateOne);
+        calendarLastComplete.setTime(dateTwo);
+
+        return calendarNow.get(Calendar.DAY_OF_YEAR) - calendarLastComplete.get(Calendar.DAY_OF_YEAR);
+
+    }
+
+    public static int getCurrentDay(String dateStart) {
+
+        Calendar calendarNow = Calendar.getInstance();
+        Calendar calendarStart = Calendar.getInstance();
+
+        String dateNow = //"02.01.20";
+                new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(calendarNow.getTime());
         SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
 
         Date dateOne = null;
@@ -28,9 +52,31 @@ public class DateChecker {
         }
         calendarNow.setTime(dateOne);
         calendarStart.setTime(dateTwo);
-        calendarStart.add(Calendar.DAY_OF_MONTH, currentDay - 1);
+
+        return calendarNow.get(Calendar.DAY_OF_YEAR) - calendarStart.get(Calendar.DAY_OF_YEAR) + 1;
+    }
+
+    public static int getDifDay(String date) {
+
+        Calendar calendarNow = Calendar.getInstance();
+        Calendar calendarStart = Calendar.getInstance();
+
+        String dateNow = //"02.01.20";
+                new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(calendarNow.getTime());
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+
+        Date dateOne = null;
+        Date dateTwo = null;
+        try {
+            dateOne = format.parse(dateNow);
+            dateTwo = format.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        calendarNow.setTime(dateOne);
+        calendarStart.setTime(dateTwo);
 
         return calendarNow.get(Calendar.DAY_OF_YEAR) - calendarStart.get(Calendar.DAY_OF_YEAR);
-
     }
+
 }
